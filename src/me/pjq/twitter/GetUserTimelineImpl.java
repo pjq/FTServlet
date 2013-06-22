f6695a53fa60029d9b06264a848b68e02ce1a838
Jianqing Peng <pengjianqing@gmail.com>
2013-06-22 11:55:00 +0800

    set count

--- src/me/pjq/twitter/GetUserTimelineImpl.java
+++ src/me/pjq/twitter/GetUserTimelineImpl.java
@@ -1,4 +1,3 @@
-
 package me.pjq.twitter;
 
 import java.io.IOException;
@@ -7,8 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import twitter4j.Paging;
 import twitter4j.ResponseList;
 import twitter4j.Status;
+import twitter4j.Twitter;
 import twitter4j.TwitterException;
 
 import me.pjq.base.GetUserTimeline;
@@ -20,57 +21,61 @@ import me.pjq.base.GetUserTimeline;
  */
 public class GetUserTimelineImpl extends GetUserTimeline {
 
-    /**
+	/**
 	 * 
 	 */
-    private static final long serialVersionUID = -492624368602865034L;
+	private static final long serialVersionUID = -492624368602865034L;
 
-    @Override
-    protected void getTimeline() {
-        // TODO Auto-generated method stub
+	@Override
+	protected void getTimeline() {
+		// TODO Auto-generated method stub
 
-    }
+	}
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        super.doGet(req, resp);
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doGet(req, resp);
 
-        if (isFromWeb()) {
-            printHeader();
-        }
+		if (isFromWeb()) {
+			printHeader();
+		}
 
-        ResponseList<Status> responseList = null;
-        try {
-            responseList = createTwitterInstance().getHomeTimeline();
-            if (isFromWeb()) {
-                out.println("<br>");
-            }
-            out.println("\n====GetHomeTimeline Response:====\n");
-            if (isFromWeb()) {
-                out.println("<br>");
-            }
-            printResponseList(responseList);
+		ResponseList<Status> responseList = null;
+		try {
+			Paging paging = new Paging();
+			paging.setCount(500);
+			Twitter twitter = createTwitterInstance();
+			responseList = twitter.getHomeTimeline(paging);
+			// responseList = createTwitterInstance().getHomeTimeline();
+			if (isFromWeb()) {
+				out.println("<br>");
+			}
+			out.println("\n====GetHomeTimeline Response:====\n");
+			if (isFromWeb()) {
+				out.println("<br>");
+			}
+			printResponseList(responseList);
 
-            responseList = createTwitterInstance().getUserTimeline();
-            if (isFromWeb()) {
-                out.println("<br>");
-            }
-            out.println("\n====GetUserTimeline Response:====\n");
-            if (isFromWeb()) {
-                out.println("<br>");
-            }
-            printResponseList(responseList);
+			responseList = createTwitterInstance().getUserTimeline(paging);
+			if (isFromWeb()) {
+				out.println("<br>");
+			}
+			out.println("\n====GetUserTimeline Response:====\n");
+			if (isFromWeb()) {
+				out.println("<br>");
+			}
+			printResponseList(responseList);
 
-        } catch (TwitterException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            out.println(e.getMessage());
-        }
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			out.println(e.getMessage());
+		}
 
-        if (isFromWeb()) {
-            printFooter();
-        }
-    }
+		if (isFromWeb()) {
+			printFooter();
+		}
+	}
 }
